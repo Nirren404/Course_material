ec0c98e582bc41f2eaacaa7c52a4c38431b656b6
Nirren404 <linajana290778@gmail.com>
2025-09-24 14:43:28 +0200

    its the first intro js excercise

---
+++ JS/js-fundamentals-introEX/script.js
@@ -0,0 +1,24 @@
+console.log("Hello, World!");
+
+let name = "Nirari";
+let age = 19;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+
+age += 1;
+isStudent = false;
+
+console.log(age);
+
+console.log(isStudent);
+
+let introtext = `Hello world, my name is ${name} I am ${age} years old. And its ${isStudent} that I am a student.`;
+
+console.log(introtext);
